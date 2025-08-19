6082ba9011546b656d4c57a3acf7f0c6d3d51613
sebugororesolomonsteven <sebugororesalomonsteven@gmail.com>
2025-08-19 19:32:48 +0300

    Created python script to create alx_book_store database

--- MySQLServer.py
+++ MySQLServer.py
@@ -2,19 +2,23 @@ import mysql.connector
 from mysql.connector import Error
 
 def create_alx_book_store_database():
+    """
+    Creates the 'alx_book_store' database in the MySQL server.
+    """
     try:
+
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
             password="Ak47??35schluppell44"
         )
-        
+
         if connection.is_connected():
             cursor = connection.cursor()
-            
+
             query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
             cursor.execute(query)
-            
+
             print("Database 'alx_book_store' created successfully!")
 
     except Error as e:
@@ -27,5 +31,4 @@ def create_alx_book_store_database():
 
 if __name__ == '__main__':
     create_alx_book_store_database()
-
 
