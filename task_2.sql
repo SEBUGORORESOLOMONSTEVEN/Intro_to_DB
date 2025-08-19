f62234c8600fc4b967c5b368d1dfd21c64459ebe
sebugororesolomonsteven <sebugororesalomonsteven@gmail.com>
2025-08-19 19:44:55 +0300

    Created all required tables for alx_book_store database

---
+++ task_2.sql
@@ -0,0 +1,42 @@
+-- Create the 'authors' table
+CREATE TABLE authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(255) NOT NULL
+);
+
+-- Create the 'books' table
+CREATE TABLE books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY(author_id) REFERENCES authors(author_id)
+);
+
+-- Create the 'customers' table
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(255) NOT NULL,
+    email_address VARCHAR(255) NOT NULL,
+    address VARCHAR(255)
+);
+
+-- Create the 'orders' table
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+);
+
+-- Create the 'order_details' table
+CREATE TABLE order_details (
+    order_detail_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY(order_id) REFERENCES orders(order_id),
+    FOREIGN KEY(book_id) REFERENCES books(book_id)
+);
+
